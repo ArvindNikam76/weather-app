10f5c323c97ed7833b233a54ddc0a69207070126
arvind.nikam <arvind.nikam@halodoc.com>
2021-04-08 23:32:01 +0530

    initial changes

---
+++ src/precache-manifest.da2c0261420276bce89aa7d890bd03ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7216d8195a76f3bf24c32c713ac0a028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "846681f6b19eb8eafab4",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "c70dc714491054885f9f",
+    "url": "/static/css/main.0bf8f541.chunk.css"
+  },
+  {
+    "revision": "846681f6b19eb8eafab4",
+    "url": "/static/js/2.e52cf27a.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.e52cf27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70dc714491054885f9f",
+    "url": "/static/js/main.cc02d690.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
